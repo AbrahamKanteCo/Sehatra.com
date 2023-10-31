7c4616cdafbea09628f71d2e7a0f0d2442c868b7
MioraRatsiresy <99732281+MioraRatsiresy@users.noreply.github.com>
2023-10-31 11:43:58 +0300

    délai

--- Sehatra/administration/interface_administration.py
+++ Sehatra/administration/interface_administration.py
@@ -262,7 +262,7 @@ def listernotification(request):
 
 
 
-def check_internet_connection(host="8.8.8.8", port=53, timeout=3):
+def check_internet_connection(host="8.8.8.8", port=53, timeout=10):
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
